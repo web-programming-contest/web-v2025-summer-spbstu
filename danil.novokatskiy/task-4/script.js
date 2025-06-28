1ebc18117ba35cc81596be4a34ea1db033478dcd
katagiri <karuk.vova2005@mail.ru>
2025-06-28 12:49:23 +0300

    [TASK-4] Part 4.1 is done

--- danil.novokatskiy/task-4/script.js
+++ danil.novokatskiy/task-4/script.js
@@ -1,7 +1,62 @@
-class student {
-  id;
-  name;
-  constructor(name) {
+"use strict";
 
+class Student {
+  constructor(id, name, grades) {
+    if (Number.isInteger(id)) {
+      this.id = id;
+    } else {
+      throw Error(`Student ${id} is not a number`);
+    }
+    if (typeof name === "string") {
+      this.name = name;
+    } else {
+      throw Error(`Name ${name} is not a string`);
+    }
+    if (grades === null) {
+      this.grades = {};
+    } else {
+      this.grades = grades;
+    }
+  }
+
+  addGrade(subject, grade) {
+    if (grade === undefined || subject === null) {
+      throw Error(`Grade ${grade} is not a number or subject ${subject} is not a string`);
+    }
+
+    for (const tmpSubject in grades) {
+      if (tmpSubject === subject) {
+        grades[tmpSubject] = grade;
+      }
+    }
+    grades[subject] = grade;
+  }
+
+  getAverageGrade() {
+    const values = Object.values(grades);
+    let result = 0;
+    for (let i = 0; i < values.length; i++) {
+      result += values[i];
+    }
+    return result / Object.keys(grades).length;
+  }
+
+  get summary() {
+    const grade = this.getAverageGrade();
+    if (grade === undefined) {
+      throw Error(`Grade ${grade} is not a number`);
+    }
+    return `Студент[${this.name}](id:[${this.id}] - средний балл:[${grade}]`;
   }
 }
+
+let grades = {
+  "math": 3,
+  "history": 4,
+  "computer science": 5,
+}
+
+let student = new Student(1, "Danil", grades);
+
+console.log(student.getAverageGrade());
+console.log(student.summary);
