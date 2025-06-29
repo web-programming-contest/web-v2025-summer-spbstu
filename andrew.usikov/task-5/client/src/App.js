3df3bfc628057eee9817955d70bae41f98055c6a
Andrew Usikov <shadowsuandru@gmail.com>
2025-06-29 05:26:20 +0300

    [TASK-5] Complete AI to play with

--- andrew.usikov/task-5/client/src/App.js
+++ andrew.usikov/task-5/client/src/App.js
@@ -1,111 +1,21 @@
 import { useState } from 'react';
+import HomePage from './HomePage/HomePage';
+import Game from './Game/Game';
 
-function Square({ value, onSquareClick }) {
-  if (value == null) {
-    value = '';
-  }
-
-  return (
-    <button className="square" onClick={onSquareClick}>
-      {value !== '' ? <img className="figure" src={value === 'X' ? '/images/cross.png' : value === 'O' ? 'images/circle.png' : null}></img> : <></>}
-    </button>
-  );
-}
-
-function Board({ xIsNext, squares, onPlay }) {
-  function handleClick(i) {
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    const nextSquares = squares.slice();
-    if (xIsNext) {
-      nextSquares[i] = 'X';
-    } else {
-      nextSquares[i] = 'O';
-    }
-    onPlay(nextSquares);
-  }
-
-  const winner = calculateWinner(squares);
-  const tie = detectFull(squares);
-  let status;
-  if (winner) {
-    status = 'Winner: ' + winner;
-  } else if (tie) {
-    status = 'Tie!';
-  } else {
-    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
-  }
+export default function App() {
+  const [gameMode, setGameMode] = useState(null);
+  const [nickname, setNickname] = useState('');
 
   return (
-    <>
-      <div className={winner != null ? "status winner" : tie ? "status tie" : "status"}>{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
-    </>
-  );
-}
-
-export default function Game() {
-  const [history, setHistory] = useState([Array(9).fill(null)]);
-  const [currentMove, setCurrentMove] = useState(0);
-  const xIsNext = currentMove % 2 === 0;
-  const currentSquares = history[currentMove];
-
-  function handlePlay(nextSquares) {
-    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
-    setHistory(nextHistory);
-    setCurrentMove(nextHistory.length - 1);
-  }
-
-  return (
-    <div className="game">
-      <div className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
-      </div>
+    <div className="app-container">
+      {gameMode ? (
+        <Game gameMode={gameMode} nickname={nickname} />
+      ) : (
+        <HomePage onSelectMode={(mode, nick) => {
+          setGameMode(mode);
+          setNickname(nick);
+        }} />
+      )}
     </div>
   );
-}
-
-function detectFull(squares) {
-  for (let i = 0; i < 9; i++) {
-    if (squares[i] === '' || squares[i] == null) {
-      return false;
-    }
-  }
-  console.log(squares);
-  return true;
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
 }
