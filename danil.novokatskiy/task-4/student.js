90ba3162f9861bb447b4634b4687d0c4e98c6928
katagiri <karuk.vova2005@mail.ru>
2025-06-30 21:06:38 +0300

    [TASK-4] Deleted unnecessary import, added main logic

--- danil.novokatskiy/task-4/student.js
+++ danil.novokatskiy/task-4/student.js
@@ -1,75 +1,93 @@
-// function addStudent() {
-//   const id = document.getElementById('ID');
-//   const name = document.getElementById('Name');
-//   alert(name + "added student");
-//   console.log(name)
-// }
+"use strict"
+
+import {Student} from "./script.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-
   let students = [];
 
-})
+  const savedStudents = localStorage.getItem("students");
+  if (savedStudents) {
+    students = JSON.parse(savedStudents);
+  }
 
-let students = [];
-const savedStudents = localStorage.getItem("students");
-if (savedStudents) {
-  students = JSON.parse(savedStudents);
-}
+  const addStudentBtn = document.querySelector("#add-student-btn");
+  addStudentBtn.addEventListener("click", async () => {
+    const id = +document.getElementById("ID").value;
+    const name = document.getElementById("Name").value;
 
-const addStudentBtn = document.querySelector("#add-student-btn");
-addStudentBtn.addEventListener("click", () => {
-  const name = document.getElementById("Name").value;
-  const id = document.getElementById("ID").value;
-  console.log(`Name: ${name}, ID: ${id}`);
-})
+    if (!name || !id) {
+      alert("Please enter a valid name or ID");
+      return;
+    }
 
-const deleteStudentBtn = document.querySelector("#delete-student-btn");
-deleteStudentBtn.addEventListener("click", () => {
-
-})
-
-function saveStudentsToLocalStorage() {
-  localStorage.setItem("students", JSON.stringify(students));
-}
-
-function addStudent(id, name) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      try {
-        if (students.some(student => student.id === id)) {
-          throw new Error("Student already exists");
-        }
-        const newStudent = {
-          ID: id,
-          name: name,
-          grades: {}
-        }
-        students.push(newStudent);
-        saveStudentsToLocalStorage()
-        resolve(`Student ${name} was added successfully!`);
-
-      } catch (err) {
-        reject(err);
-      }
-    }, 1000)
+    try {
+      await addStudent(id, name);
+      document.getElementById("ID").value = "";
+      document.getElementById("Name").value = "";
+    } catch (err) {
+      alert(err);
+    }
   })
-}
 
-function deleteStudent(id) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      try {
-        const index = students.findIndex(student => student.id === id);
-        if (index === -1) {
-          throw new Error("Student is found by this id");
-        }
-        const deletedStudent = students.splice(index, 1)[0];
-        saveStudentsToLocalStorage();
-        resolve(`Student ${name} was deleted successfully!`);
-      } catch (err) {
-        reject(err);
-      }
-    }, 1000)
+  const deleteStudentBtn = document.querySelector("#delete-student-btn");
+  deleteStudentBtn.addEventListener("click", async () => {
+    const id = +document.getElementById("ID").value;
+
+    if (!id) {
+      alert("Please enter a valid id");
+      return;
+    }
+
+    try {
+      await deleteStudent(id);
+      document.getElementById("ID").value = "";
+      document.getElementById("Name").value = "";
+    } catch (err) {
+      alert(err);
+    }
   })
-}
+
+
+  function saveStudentsToLocalStorage() {
+    localStorage.setItem("students", JSON.stringify(students));
+  }
+
+  function addStudent(id, name) {
+    return new Promise(() => {
+      setTimeout(() => {
+        try {
+          if (students.some(student => student.id === id)) {
+            throw new Error("Student already exists");
+          }
+          const newStudent = new Student(id, name);
+          students.push(newStudent);
+          saveStudentsToLocalStorage();
+          alert("Student added successfully");
+        } catch (err) {
+          alert(err);
+        }
+      }, 1000)
+    })
+  }
+
+  function deleteStudent(id) {
+    return new Promise(() => {
+      setTimeout(() => {
+        try {
+          const index = students.findIndex(student => student.id === id);
+          if (index === -1) {
+            throw new Error("Student is found by this id");
+          }
+          const deletedStudent = students.splice(index, 1)[0];
+          saveStudentsToLocalStorage();
+          alert("Student deleted");
+        } catch (err) {
+          alert(err);
+        }
+      }, 1000)
+    })
+  }
+});
+
+
+
