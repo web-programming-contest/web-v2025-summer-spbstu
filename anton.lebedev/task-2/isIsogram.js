a68a145840cd16c9dbffaba1aa4404157ac583f0
IastWish <antony_lebedevv@mail.ru>
2025-06-30 18:19:25 +0300

    [TASK-2] Implementation of isIsogram func

--- anton.lebedev/task-2/isIsogram.js
+++ anton.lebedev/task-2/isIsogram.js
@@ -1,5 +1,19 @@
-function func() {
-    console.log("!")
+"use strict";
+
+function isLetter(char) {
+    return char.toLowerCase() !== char.toUpperCase();
 }
 
-func();
+function isIsogram(str) {
+    let lowStr = str.toLowerCase();
+    let uniqueLetters = new Set();
+    for (let char of lowStr) {
+        if (isLetter(char)) {
+            if (uniqueLetters.has(char)) {
+                return false;
+            }
+            uniqueLetters.add(char);
+        }
+    }
+    return true;
+}
