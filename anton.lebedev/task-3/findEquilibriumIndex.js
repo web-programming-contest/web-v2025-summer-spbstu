dc3ca0e25b8464baf281ba5de61da0f553f89128
IastWish <antony_lebedevv@mail.ru>
2025-07-02 17:41:19 +0300

    [TASK-3] Implementation of findEquilibriumIndex func

--- anton.lebedev/task-3/findEquilibriumIndex.js
+++ anton.lebedev/task-3/findEquilibriumIndex.js
@@ -1,5 +1,24 @@
 "use strict";
 
 function findEquilibriumIndex(arr) {
-    return -1;
+    let frontInd = 0;
+    let backInd = arr.length - 1;
+    let frontSum = 0;
+    let backSum = 0;
+    while (frontInd <= backInd) {
+        if (frontSum > backSum) {
+            backSum += arr[backInd];
+            --backInd;
+        }
+        else {
+            frontSum += arr[frontInd];
+            ++frontInd;
+        }
+    }
+    if (frontSum === backSum && frontInd !== 0) {
+        return frontInd;
+    }
+    else {
+        return -1;
+    }
 }
