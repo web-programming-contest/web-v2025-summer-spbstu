7cad64d19a45d771b429f9bfb89e1c84f1809521
TheOneFoxAgo <nikitad2005@gmail.com>
2025-07-03 16:29:22 +0300

    [TASK-4] Recipe class implementation

---
+++ nikita.demidenko/task-4/recipe.js
@@ -0,0 +1,28 @@
+class recipe {
+    title; // String
+    ingredients; // String array
+    steps; // String array
+
+    constructor(title, ingredients, steps) {
+        this.title = title;
+        this.ingredients = ingredients;
+        this.steps = steps;
+    }
+
+    addIngredient(ingredient) {
+        this.ingredients.push(ingredient)
+    }
+
+    removeIngredient(ingredient) {
+        let ingredientIdx = this.ingredients.indexOf(ingredient);
+        if (ingredientIdx == -1) {
+            return false;
+        }
+        this.ingredients.splice(ingredientIdx, 1);
+        return true;
+    }
+    
+    get ingredientCount() {
+        return this.ingredients.length;
+    }
+}
