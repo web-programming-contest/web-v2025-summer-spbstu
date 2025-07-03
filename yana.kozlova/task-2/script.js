20b129ba3fc4f96bdb701099a7bc34a937f51f0b
AuntMotYA <ikozyanag.2004@gmail.com>
2025-07-03 03:37:00 +0300

    [TASK-2] Finished js

---
+++ yana.kozlova/task-2/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function wordReversal(line) {
+    let line_new = line.split(' ');
+    for (let i = 0; i < line_new.length; i++) {
+        line_new[i] = line_new[i].split('').reverse().join('');
+    }
+    return line_new.join(' ');
+}
+
+let line = prompt('Строка, которую надо перевернуть:');
+console.log('Исходная строка: ' + line);
+console.log('Результат: ' + wordReversal(line))
