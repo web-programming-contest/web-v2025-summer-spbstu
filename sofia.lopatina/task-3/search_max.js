6d9398bfac39d744fb79582e5feaef51d9d61929
SofiyLop <lopatina.sa@edu.spbstu.ru>
2025-07-04 16:54:56 +0300

    [TASK-3] Initial html and js

---
+++ sofia.lopatina/task-3/search_max.js
@@ -0,0 +1,20 @@
+"use strict";
+
+//let arr = [];
+//let k;
+let arr = [1, 5, 6, 7, 8, 4, 6, 3, 6, 8];
+let k = 3;
+try {
+    maxSlidingWindow(arr, k);
+} catch (err) {
+    console.log(err);
+}
+
+function maxSlidingWindow(arr, k) {
+    let len_arr = arr.length
+    if (len_arr == 0) {
+        throw {name: 'EmptyArrError', message: 'Массив пуст'};
+    } else if (k >= len_arr && k <= 0) {
+        throw {name: 'LengthError', message: 'Задана некорректная длина подмассива'};
+    }
+}
