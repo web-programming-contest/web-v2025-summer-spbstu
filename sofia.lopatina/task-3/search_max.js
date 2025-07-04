8a125312060b85ad4c3c9bc1c8c94d8afd7b89e9
SofiyLop <lopatina.sa@edu.spbstu.ru>
2025-07-04 19:12:16 +0300

    [TASK-3] Add input analysis

--- sofia.lopatina/task-3/search_max.js
+++ sofia.lopatina/task-3/search_max.js
@@ -1,20 +1,50 @@
 "use strict";
 
-//let arr = [];
-//let k;
-let arr = [1, 5, 6, 7, 8, 4, 6, 3, 6, 8];
-let k = 3;
-try {
-    maxSlidingWindow(arr, k);
-} catch (err) {
-    console.log(err);
+let button = document.querySelector('#button');
+button.addEventListener('click', startSearch);
+
+function startSearch() {
+    const input_arr = document.querySelector('#input_arr').value;
+    const input_k = document.querySelector('#input_k').value;
+    let arr = [];
+    let num_arr = [];
+    let k;
+
+    try {
+        arr = input_arr.replaceAll(' ', '');
+        arr = arr.split(',');
+
+        for (let i = 0; i < arr.length; i++)
+        {
+            if (arr[i] == '') {
+                continue;
+            }
+
+            let elem = Number(arr[i]);
+            if (isNaN(elem)) {
+                throw {name: 'InvalidNumError', message: 'Введено не число'};
+            }
+            num_arr.push(elem);
+        }
+
+        k = Number(input_k);
+        if (isNaN(k)) {
+            throw {name: 'InvalidNumError', message: 'Введено не число'};
+        }
+        console.log(num_arr);
+        console.log(k);
+
+        //maxSlidingWindow(arr, k);
+    } catch(err) {
+        console.log(err);
+    }
 }
 
 function maxSlidingWindow(arr, k) {
     let len_arr = arr.length
     if (len_arr == 0) {
         throw {name: 'EmptyArrError', message: 'Массив пуст'};
-    } else if (k >= len_arr || k <= 0) {
+    } else if (k > len_arr || k <= 0) {
         throw {name: 'LengthError', message: 'Задана некорректная длина подмассива'};
     }
 
