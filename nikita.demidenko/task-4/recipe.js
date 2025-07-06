c451e71cbd6eeb114df5bbb8098e9310eeb4f2a9
TheOneFoxAgo <nikitad2005@gmail.com>
2025-07-06 21:21:50 +0300

    [TASK-4] Added required buttons to recipes

--- nikita.demidenko/task-4/recipe.js
+++ nikita.demidenko/task-4/recipe.js
@@ -10,19 +10,35 @@ class recipe {
     }
 
     addIngredient(ingredient) {
-        this.ingredients.push(ingredient)
+        this.#addItem("ingredients", ingredient);
     }
 
     removeIngredient(ingredient) {
-        let ingredientIdx = this.ingredients.indexOf(ingredient);
-        if (ingredientIdx == -1) {
-            return false;
-        }
-        this.ingredients.splice(ingredientIdx, 1);
-        return true;
+        return this.#removeItem("ingredients", ingredient);
+    }
+
+    addStep(step) {
+        this.#addItem("steps", step);
+    }
+
+    removeStep(step) {
+        return this.#removeItem("steps", step)
     }
 
     get ingredientCount() {
         return this.ingredients.length;
+    }
+
+    #addItem(key, item) {
+        this[key].push(item);
+    }
+
+    #removeItem(key, item) {
+        let idx = this[key].indexOf(item);
+        if (idx === -1) {
+            return false;
+        }
+        this[key].splice(idx, 1);
+        return true;
     }
 }
