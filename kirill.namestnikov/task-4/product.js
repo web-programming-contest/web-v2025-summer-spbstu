a10752d15b107a7f4ecaa984140fcc61fc600d7c
kirillnamestnikov <kir.namestnikov@gmail.com>
2025-07-08 14:02:09 +0300

    [TASK-4] Added Product class

---
+++ kirill.namestnikov/task-4/product.js
@@ -0,0 +1,28 @@
+class Product{
+    id;
+    name;
+    categories;
+    price;
+    constructor(id, name, categories = [], price){
+        this.id = id;
+        this.name = name;
+        this.categories = categories;
+        this.price = price;
+    }
+
+    addCategory(category){
+        this.categories.push(category);
+    }
+
+    removeCategory(category){
+        this.categories = this.categories.filter(c => c !== category);
+    }
+    get categoryCount(){
+        return this.categories.length;
+    }
+}
+
+let product = new Product(0, "Apple", [], 100);
+console.log(product.categoryCount);
+product.addCategory("Fruits");
+console.log(product.categoryCount);
